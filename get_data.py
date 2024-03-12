56a8197b4046bcf6a313c1dbceef0c08d5d2581d
Lyreon <78847209+lyrenox@users.noreply.github.com>
2024-03-12 22:39:23 +0800

    Modify script to generate token on launch

--- get_data.py
+++ get_data.py
@@ -1,19 +1,41 @@
 # Pokemon Cafe Remix database by TalonMallon
 # https://docs.google.com/spreadsheets/d/1SxDpFfGX6VsFucnUyCf8rcw_puSOWGK8EBw3aHHEj5s
 
+import os.path
+
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 import json
 
+# To get your own token, see instructions on https://developers.google.com/sheets/api/quickstart/python
 def get_values(spreadsheet_id, range_name):
     """
+    Creates a token if one does not exist.
     Creates the batch_update the user has access to.
-    Load pre-authorized user credentials from the environment.
     """
-    creds = Credentials.from_authorized_user_file("token.json") 
-    # To get your own token, see instructions on https://developers.google.com/sheets/api/quickstart/python
+    SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+    
+    creds = None
+
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
     try:
         service = build("sheets", "v4", credentials=creds)
 
